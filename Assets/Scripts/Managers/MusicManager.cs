21af6f3c9da0bcfa4749e5311297f92b704c4186
Getsumi3 <tomiuc117@gmail.com>
2018-11-14 01:57:03 +0200

    Initial commit

---
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
+
+    int sceneID;
+
+    void Start()
+    {
+        //OnSceneLoaded(0);
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        int newSceneID = SceneManager.GetActiveScene().buildIndex;
+        print("SCENE ID: " + sceneID);
+        print("NEW SCENE ID: " + newSceneID);
+        if (newSceneID != sceneID)
+        {
+            sceneID = newSceneID;
+            Invoke("PlayMusic", .2f);
+        }
+        else {
+            sceneID = newSceneID;
+            Invoke("PlayMusic", .2f);
+        }
+    }
+
+    void PlayMusic()
+    {
+        print("PLAY!");
+        AudioClip clipToPlay = null;
+
+        if (sceneID == 0)
+        {
+            clipToPlay = menuTheme;
+        }
+        else if (sceneID == 1)
+        {
+            clipToPlay = mainTheme;
+        }
+
+        if (clipToPlay != null)
+        {
+            AudioManager.instance.PlayMusic(clipToPlay, 2);
+            Invoke("PlayMusic", clipToPlay.length);
+        }
+
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GunController))]
+public class Player : LivingEntity
+{
+    GunController gunController;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void Awake()
+    {
+        gunController = GetComponent<GunController>();
+        OnNewWave(1);
+    }
+
+    void OnNewWave(int waveNumber)
+    {
+        health = startingHealth;
+        gunController.EquipGun(waveNumber - 1);
+    }
+
+    void Update()
+    {
+        // Weapon input
+        if (Input.GetMouseButton(0))
+        {
+            gunController.OnTriggerHold();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            gunController.OnTriggerRelease();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gunController.Reload();
+        }
+
+        if (transform.position.y < -10)
+        {
+            TakeDamage(health);
+        }
+    }
+
+    public override void Die()
+    {
+        //AudioManager.instance.PlaySound("Player Death", transform.position);
+        base.Die();
+    }
+
+
+}
